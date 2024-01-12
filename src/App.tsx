44bc42bf4f2a5c0fc43f74d4596dd5234bb162af
Nicolas Doyen <nico-foot10@hotmail.com>
2024-01-12 03:11:44 +0100

    refactor input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-import { WeatherApi } from "./API/WeatherApi";
+// import { WeatherApi } from "./API/WeatherApi";
 import { Input } from "./components/Input";
 
 
 export const App = () => {
-  const [input, setInput] = useState<string>();
-  const lat = "50.6324127197283";
-  const lon = "5.568375721181737";
+  const [input, setInput] = useState<string>("");
+  // const lat = "50.6324127197283";
+  // const lon = "5.568375721181737";
 
   const handleChangeInput = (value: string) => setInput(value)
 
   useEffect(() => {
-    WeatherApi.getCurrentWeather(lat, lon).then((data) => console.log(data))
+    // WeatherApi.getCurrentWeather(lat, lon).then((data) => console.log(data))
   }, [])
 
 
